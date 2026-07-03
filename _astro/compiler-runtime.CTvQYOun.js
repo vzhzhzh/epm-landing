e98ebea926719d12cf9cdaf54bddf2cbf43a7488
Denis Khrykin <denis@khrykin.dev>
2026-07-03 09:46:17 +0400

    feat: redesign

---
+++ _astro/compiler-runtime.CTvQYOun.js
@@ -0,0 +1,17 @@
+import{r as v}from"./index.CMF_dOlt.js";var s={exports:{}},t={};/**
+ * @license React
+ * react-jsx-runtime.production.js
+ *
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */var _;function E(){if(_)return t;_=1;var u=Symbol.for("react.transitional.element"),i=Symbol.for("react.fragment");function p(l,r,e){var n=null;if(e!==void 0&&(n=""+e),r.key!==void 0&&(n=""+r.key),"key"in r){e={};for(var o in r)o!=="key"&&(e[o]=r[o])}else e=r;return r=e.ref,{$$typeof:u,type:l,key:n,ref:r!==void 0?r:null,props:e}}return t.Fragment=i,t.jsx=p,t.jsxs=p,t}var m;function d(){return m||(m=1,s.exports=E()),s.exports}var q=d(),R={exports:{}},a={};/**
+ * @license React
+ * react-compiler-runtime.production.js
+ *
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */var x;function f(){if(x)return a;x=1;var u=v().__CLIENT_INTERNALS_DO_NOT_USE_OR_WARN_USERS_THEY_CANNOT_UPGRADE;return a.c=function(i){return u.H.useMemoCache(i)},a}var c;function T(){return c||(c=1,R.exports=f()),R.exports}var A=T();export{A as c,q as j};
